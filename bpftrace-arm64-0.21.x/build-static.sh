1561f99a7d272294f674062b2c685204b9a06401
liuxinquan <546472201@qq.com>
2025-01-22 16:18:18 +0800

    update build

--- bpftrace-arm64-0.21.x/build-static.sh
+++ bpftrace-arm64-0.21.x/build-static.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -eu
-docker build --network host  --build-arg BUILD_PATH="$(pwd)/build-static" --build-arg TYPE=Release -t bpftrace-builder  -f Dockerfile-static.arm64 .
-docker run --network host --rm -it -u $(id -u):$(id -g) -v $(pwd):$(pwd) bpftrace-builder
+docker build --network host -t bpftrace-builder  -f Dockerfile-static.arm64 .
+docker run --network host --rm -it -u $(id -u):$(id -g) -v $(pwd):$(pwd) bpftrace-builder "$(pwd)/build-static" Release "$@"
